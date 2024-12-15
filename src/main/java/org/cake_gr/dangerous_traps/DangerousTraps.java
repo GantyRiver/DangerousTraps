5c8eda113f440bb43b76c5ac2bf4ba73efb3ff92
westernat <westernat233@163.com>
2024-12-15 16:37:16 +0800

    initial commits

---
+++ src/main/java/org/cake_gr/dangerous_traps/DangerousTraps.java
@@ -0,0 +1,17 @@
+package org.cake_gr.dangerous_traps;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Mod(DangerousTraps.MODID)
+public class DangerousTraps {
+    public static final String MODID = "dangerous_traps";
+    public static final Logger LOGGER = LoggerFactory.getLogger("Dangerous Traps");
+
+    public DangerousTraps(IEventBus modEventBus, ModContainer modContainer) {
+
+    }
+}
